c81a2ecc4e5f199ab086b2c81a0fe9bd268b0c4c
b4ruch <12469509+b4ruch@users.noreply.github.com>
2021-06-21 07:47:25 +0000

    adds API gateway URL

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://c33f805c.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
